2d5daf63a8ac11e7ffbf8dfc52d5de8b3c387d3a
Kirk Benell <kirk@EX-BENELL.local>
2018-06-18 10:10:21 -0600

    support for spectacle

---
+++ boards/sfe-spectacle/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define __SAMD21G18A__ 1
+
+#define VENDOR_NAME "Sparkfun"
+#define PRODUCT_NAME "Spectacle Directory Board"
+#define VOLUME_LABEL "SPECTBOOT"
+#define INDEX_URL "https://www.sparkfun.com/products/13912"
+#define BOARD_ID "SAMD21G18A-Sparkfun-Spectacle-v0"
+
+#define USB_VID 0x1B4F
+#define USB_PID 0x0D21
+
+#define LED_PIN PIN_PA17
+
+
+#endif
